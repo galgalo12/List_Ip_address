89433092543a5444687c6a594586bbd4080e3880
ABDIFATAH GALGALO <47960322+galgalo12@users.noreply.github.com>
2025-03-07 03:54:31 -0800

    Add files via upload
    This Python script performs the following tasks:
    
    Defines a list of known malicious IP addresses: Malicious_Ipaddress contains IPs identified as malicious.
    
    Sorts and displays these malicious IPs: It prints the Malicious_Ipaddress list in ascending order.
    
    Lists all login attempt IPs: ip_addresses represents IPs attempting to log in.
    
    Prints each login attempt IP: Iterates through ip_addresses and prints each IP.
    
    Defines an allowed list of IP addresses: allow_list contains IPs permitted to access the system.
    
    Checks each login attempt against the allowed list:
    
    If an IP is in allow_list, it prints that the IP is allowed.
    If an IP is not in allow_list, it increments the malicious IP count and prints a warning to block the IP in the firewall.
    Displays the total number of malicious IP addresses detected: Prints the count of IPs that should be blocked.

---
+++ Malicious_Ip_address.py
@@ -0,0 +1,42 @@
+# List of known malicious IP addresses
+Malicious_Ipaddress = [
+    "192.168.142.245", "192.168.109.50", "192.168.86.232",
+    "192.168.131.147", "192.168.205.12", "192.168.200.48"
+]
+
+# Sort and display failed login attempts in ascending order
+print("Sorted failed login attempts:", sorted(Malicious_Ipaddress))
+print("\n")
+
+# List of login attempt IPs
+ip_addresses = [
+    "192.168.142.245", "192.168.109.50", "192.168.86.232", "192.168.131.147", "192.168.205.12", "192.168.200.48"
+]
+
+# Print all login attempt IPs
+print("Malicious Login attempt IPs:")
+print("\n")
+for ip in ip_addresses:
+    print(ip)
+print("\n")
+
+# Allowed list of IP addresses
+allow_list = [
+    "192.168.243.140", "192.168.205.12", "192.168.151.162", "192.168.178.71", "192.168.86.232", 
+    "192.168.3.24", "192.168.170.243", "192.168.119.173"
+]
+
+# Counter for malicious IP addresses
+malicious_count = 0
+
+# Check if login attempt IPs are allowed or should be blocked
+for ip in ip_addresses:
+    if ip in allow_list:
+        print(f"{ip} This IP address is allowed.\n")  # Allowed IPs
+    else:
+        malicious_count += 1  # Increment malicious IP count
+        print(f"⚠️ {ip} This IP is malicious and should be BLOCKED in the firewall.\n")  # Malicious IPs
+   
+
+# Print the total count of malicious IP addresses
+print(f"Total malicious IP addresses detected: {malicious_count}")
